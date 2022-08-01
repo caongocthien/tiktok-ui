d99d13b7a908f5925b750b2b59f9e0cb6f20ddb2
Thien Cao <caongocthien36@gmail.com>
2022-08-01 21:03:58 +0700

    Config router page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layout";
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+      <Routes>
+        {publicRoutes.map((r, index) => {
+          // const Layout = r.layout === null ? Fragment : DefaultLayout;
+          let Layout = DefaultLayout;
+          if (r.layout) {
+            Layout = r.layout;
+          } else if (r.layout === null) {
+            Layout = Fragment;
+          }
+          return (
+            <Route
+              key={index}
+              path={r.path}
+              element={
+                <Layout>
+                  <r.component />
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
   );
 }
 
